35c56c3c832705c8db6348ae24b4640eeaa94b94
Kush <kuriaisac@gmail.com>
2024-08-31 16:13:02 +0300

    first commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
-# your code goes here!
+# create a class
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = ''.join(sorted(self.word))
+
+    def match(self, list_anagram):
+        matches = []
+        for word in list_anagram:
+
+            if ''.join(sorted(word.lower())) == self.sorted_word and word.lower() != self.word:
+                matches.append(word)
+        return matches
+    
+pikachu = Anagram('listen')
+words = ["enlists", "google", "inlets", "banana"]
+print(pikachu.match(words))
+
+
+        
